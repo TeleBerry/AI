7a38c2a2c5028076be3fa7e856ff5c584f231a47
TeleBerry <Teleberry03@gmail.com>
2021-05-05 15:01:38 +0800

    update_train

--- dog_and_cat_train.py
+++ dog_and_cat_train.py
@@ -1,9 +1,8 @@
-import ReadMyOwnData  # 讀取自己建的 py檔
-from keras.layers.convolutional import Conv2D
 import numpy as np
+from keras.layers.convolutional import Conv2D
+import ReadMyOwnData  # 讀取自己建的 py檔
+from sklearn.datasets import make_blobs
 import tensorflow as tf
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
 
 
 epoch = 15  # 訓練15期
@@ -26,8 +25,8 @@ def bias_variable(shape):  # initial bias
     return tf.Variable(initial)
 
 
-x = tf.placeholder(tf.float32, [batch_size, 128, 128, 3])
-y_ = tf.placeholder(tf.float32, [batch_size, 2])
+x = tf.compat.v1.placeholder(tf.float32, [batch_size, 128, 128, 3])
+y_ = tf.compat.v1.placeholder(tf.float32, [batch_size, 2])
 
 
 # sess = tf.InteractiveSession()
@@ -65,7 +64,7 @@ b_fc1 = bias_variable([1024])
 h_fc1 = tf.nn.relu(tf.matmul(reshape, W_fc1) + b_fc1)
 
 # dropout
-keep_prob = tf.placeholder(tf.float32)
+keep_prob = tf.compat.v1.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 W_fc2 = weight_variable([1024, 2])
@@ -84,18 +83,15 @@ img, label = ReadMyOwnData.read_and_decode("dog_and_cat_train.tfrecords")
 img_test, label_test = ReadMyOwnData.read_and_decode(
     "dog_and_cat_test.tfrecords")
 
-# 使用 shuffle_batch可以随機打亂输入
 img_batch, label_batch = tf.train.shuffle_batch([img, label],
                                                 batch_size=batch_size, capacity=2000,
                                                 min_after_dequeue=1000)
 img_test, label_test = tf.train.shuffle_batch([img_test, label_test],
                                               batch_size=batch_size, capacity=2000,
                                               min_after_dequeue=1000)
-
 init = tf.initialize_all_variables()
 t_vars = tf.trainable_variables()
 print(t_vars)
-
 with tf.Session() as sess:
     sess.run(init)
     coord = tf.train.Coordinator()
@@ -116,5 +112,6 @@ with tf.Session() as sess:
                       x: val, y_: l, keep_prob: 1})
     print(y)
     print("test accuracy: [%.8f]" % (acc))
+
     coord.request_stop()
     coord.join(threads)
