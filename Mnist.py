dcb074cff23e46ac3da2cae33fa3da95a78e0ed2
TeleBerry <Teleberry03@gmail.com>
2021-05-27 15:43:49 +0800

    update

--- Mnist.py
+++ Mnist.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
+# import tensorflow as tf
+from tensorflow import input_data
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 mnist = input_data.read_data_sets("./MNIST_data/", one_hot=True)
 
 
