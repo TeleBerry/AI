a96567b74f1ac0aceecdb8cb27b200368e4aeae1
TeleBerry <Teleberry03@gmail.com>
2021-09-17 11:56:16 +0800

    update

--- catdog.py
+++ catdog.py
@@ -1,36 +1,31 @@
-# ---------keras資料下載--------------------------------------------------
-import kears
-from kears.preprocessing.image import ImageDataGenerator
-from kears_tqdm import TQDMNotebookCallback
-from kears.models import Sequential
-from kears.layers import Dense
-from kears.layers import Dropout
-from kears.layers import Flatten
-from kears.constraints import maxnorm
-from kears.optimizers import SGD
-from kears.layers.convolutional import Conv2D
-from kears.layers.convolutional import MaxPooling2D
-from kears.utils import np_utils
-from kears.callbacks import Callback
-# ---------keras資料下載--------------------------------------------------
-
 # ---------準備資料--------------------------------------------------
-from matplotlib import pyplot as plt
-from PIL import Image
-import numpy as np
-import os
-import cv2
-from tqdm import tqdm_notebook
-from random import shuffle
-import shutil
+from keras.callbacks import Callback
+from keras.utils import np_utils
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.convolutional import Conv2D
+from keras.optimizers import SGD
+from keras.constraints import maxnorm
+from keras.layers import Flatten
+from keras.layers import Dropout
+from keras.layers import Dense
+from keras.models import Sequential
+from keras_tqdm import TQDMNotebookCallback
+from keras.preprocessing.image import ImageDataGenerator
+import keras
 import pandas as pd
-
-%matplotlib inline
-
+import shutil
+from random import shuffle
+from tqdm import tqdm_notebook
+import cv2
+import os
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+# %matplotlib inline
 # ---------準備資料--------------------------------------------------
+
 
 # ---------建立引數--------------------------------------------------
-
 
 def organize_datasrts(path_to_data, n=4000, ratio=0.2):
     files = os.listdir(path_to_data)
@@ -67,13 +62,71 @@ def organize_datasrts(path_to_data, n=4000, ratio=0.2):
 
 batch_size = 32
 
+# 標準化處理
 train_datagen = ImageDataGenerator(
-    rescale=1/255., shear_range=0.2, horizontal_filp=True)
+    rescale=1/255.,  # rescale放縮因子，調整像素
+    shear_range=0.2,  # 錯切變換，讓座標保持不變
+    horizontal_flip=True  # 水平翻轉
+)
 
 val_datagen = ImageDataGenerator(rescale=1/255.)
 
 # ---------建立引數--------------------------------------------------
 
-# ---------資料增強--------------------------------------------------
+# 生成器，要讓資料增強
+train_generator = train_datagen.flow_from_directory(
+    './data/train/', target_size=(150, 150), batch_size=batch_size, class_mode='categorical')
 
-# ---------資料增強--------------------------------------------------
+validation_generator = val_datagen.flow_from_directory(
+    './data/train/', target_size=(150, 150), batch_size=batch_size, class_mode='categorical')
+
+# Found 20000 images belonging to 2 classes.
+# Found 5000 images belonging to 2 classes.
+
+
+# 卷積/池化層(3)---------------------------------------------------------------------------------------------------
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), inpur_shape=(150, 150, 3),
+          padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# 全連接層(2)--------------------------------------------------------------
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
+
+# 隨機梯度下降法-----------------------------------------------------------------------
+epochs = 50
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+mosel.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Callback for loss logging per ephoch
+
+
+class LossHistory(Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.val_losses = []
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
+
+
+history = LossHistory()
+
+# Callback for early stopping the training
+early_stopping-keras.callbacks.EarlyStopping
+(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
